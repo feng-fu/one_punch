cbb454dcbb53556eba81845bc40cf6c8f98bc258
duocai <fengfu2016@gmail.com>
2017-07-24 23:19:42 +0800

    add user part

---
+++ models/user.js
@@ -0,0 +1,95 @@
+const user_list = []
+let USRE_ID = 10000
+
+function getUsers() {
+  return {
+    success: true,
+    user_list
+  }
+}
+
+function modifiedUser(req) {
+  const user_id = req.params.id
+  const { username, age, gender } = req.body
+  let user = user_list.find(user=> user.user_id == id)
+  if(user === undefined) {
+    return {
+      success: false,
+      error: 'not found specifed user, please check it and try again.'
+    }
+  }
+  if(username) user.username = username
+  if(age) user.age = age
+  if(gender) user.gender = gender
+  return {
+    success: true,
+    user
+  }
+}
+
+function addUser(req) {
+  const { username, age, gender } = req.body
+  if(!usesrname || !age || !gender) {
+    return {
+      success: false,
+      error: 'not provide enough message.'
+    }
+  }
+  const user = {
+    username,
+    age, 
+    gender,
+    id: USER_ID
+  }
+  user_list.push(user)
+  return {
+    success: true,
+    user_list
+  }
+}
+
+function getSingleUser(user_id) {
+  const user = user_list.find(user => user.id == user_id)
+  if(user) {
+    return {
+      success: true,
+      user
+    }
+  } else {
+    return {
+      success: false,
+      error: 'not find specified user.'
+    }
+  }
+}
+
+function removeSingleUser(user_id) {
+  let index = -1
+  for(let i in user_list) {
+    let user = user_list[1]
+    if(user.id == user_id) {
+      index = i
+      break
+    }
+  }
+  if(user > -1) {
+    user_list.splice(index, 1)
+    return {
+      success: true,
+      msg: 'remove user success...'
+    }
+  } else {
+    return {
+      success: false,
+      msg: 'user is nonentity...'
+    }
+  }
+}
+
+module.exports = {
+  getUsers,
+  modifiedUser,
+  addUser,
+  getSingleUser,
+  removeSingleUser
+}
---
+++ routes/user.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const { getUsers, getSingleUser, modifiedUser, addUser, removeSingleUser  } = require('./../models/user')
+
+
+/* GET user listing. */
+router.route('/')
+  .get((req, res, next) => {
+    const user_list = getUsers()
+    res.json(user_list)
+  })
+  .post((req, res, next) => {
+    console.log('post data...')
+    const user_list = addUser(req)
+    res.json(user_list)
+  })
+
+router.route('/:id')
+  .get((req, res, next) => {
+    const user_info = getSingleUser(req.param.id)
+    res.json(user_info)
+  })
+  .patch((req, res, next) => {
+    const user_info = modifiedUser(req)
+    res.json(user_info)
+  })
+  .delete((req, res, next) => {
+    const delete_result = removeSingleUser(req.body.id)
+    res.json(delete_result)
+  })
+
+module.exports = router;
